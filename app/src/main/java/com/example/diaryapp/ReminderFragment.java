a83447273b24dbf186a770240c2589d6743ae8af
Wenyu <sandy3809@gmail.com>
2025-01-06 02:16:14 +0800

    add Reminder function (not finished)

--- app/src/main/java/com/example/diaryapp/ReminderFragment.java
+++ app/src/main/java/com/example/diaryapp/ReminderFragment.java
@@ -9,7 +9,6 @@ import androidx.fragment.app.Fragment;
 
 public class ReminderFragment extends Fragment {
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
